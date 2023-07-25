035ea68aa69f1c5b1b9c553eed11b74da14cca54
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-07-25 22:04:44 +0600

    implemented code generation model

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+import { useForm } from 'react-hook-form';
+import { Heading } from '@/components/heading';
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import React, { useState } from 'react';
+import { CodeIcon} from "lucide-react";
+import ReactMarkdown from "react-markdown";
+import { formSchema } from './constants';
+import { FormControl, FormField, FormItem, Form } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import axios from 'axios'
+import { useRouter } from 'next/navigation';
+import { ChatCompletionRequestMessage } from 'openai';
+import { Loader } from '@/components/loader';
+import { cn } from '@/lib/utils';
+import { Empty } from '@/components/empty';
+import { UserAvatar } from '@/components/user-avatar';
+import { BotAvatar } from '@/components/bot-avatar';
+
+const Code = () => {
+
+    const router = useRouter()
+    const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([])
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: ""
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const userMessage: ChatCompletionRequestMessage = {
+                role: "user",
+                content: values.prompt
+            }
+
+            const newMessages = [...messages, userMessage];
+            const response = await axios.post("/api/code", { messages: newMessages });
+            setMessages((current) => [...current, userMessage, response.data]);
+            form.reset()
+        } catch (error: any) {
+            console.log(error)
+        } finally {
+            router.refresh()
+        }
+    }
+
+    return (
+        <div>
+            <Heading title="Code Generation"
+                description="Generate code with descriptive text."
+                icon={CodeIcon}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10" />
+            <div className='px-4 lg:px-8'>
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+                        >
+                            <FormField
+                                control={form.control}
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="Simple toggle button using react hooks."
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+
+                <div className='space-y-4 mt-4'>
+                    {
+                        isLoading && (<div className='p-8 rounded-lg w-full flex items-center justify-center bg-muted'>
+                            <Loader />
+                        </div>)
+                    }
+                    {messages.length === 0 && !isLoading && (
+                        <Empty label="No conversation started." />
+                    )}
+
+
+                    <div className='flex flex-col-reverse gap-y-4'>
+                        {
+                            messages && messages.map((message, idx) => <div
+                                className={cn("p-8 w-full flex items-start gap-x-8 rounded-lg", message.role == "user" ? "bg-white border border-black/10" : "bg-muted")}
+                                key={idx}>
+                                {message?.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                                <ReactMarkdown components={{
+                                    pre: ({ node, ...props }) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props} />
+                                        </div>
+                                    ),
+                                    code: ({ node, ...props }) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props} />
+                                    )
+                                }} className="text-sm overflow-hidden leading-7">
+                                    {message.content || ""}
+                                </ReactMarkdown>
+                            </div>)
+                        }
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Code;
